dbd47c5d27ddade3c7ccdb5f212f2ae28235df43
kbcruz6 <agustintcruz@gmail.com>
2022-12-12 16:03:25 -0300

    123

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -11,12 +11,12 @@ const Weather = ({ data }) => {
       className="relative flex flex-col justify-between max-w-[400px] w-full m-auto p-4 text-white z-10"
     >
       {/*//! Title */}
-      <p className="text-6xl mb-10 text-center font-bold text-shadow">
+      <p className="text-6xl my-6 text-center font-bold text-shadow">
         {data.name}
       </p>
       {/*//! Right now weather  */}
       <div className=" mb-10 flex justify-between items-center ">
-        <div className="bg-white/30 rounded-2xl flex flex-col items-center font-bold text-shadow p-4 shadow-lg shadow-black">
+        <div className="bg-white/30 rounded-2xl flex flex-col items-center font-bold text-shadow p-4 shadow-md shadow-black">
           <Image
             src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
             alt="/"
@@ -41,23 +41,11 @@ const Weather = ({ data }) => {
         {/*//! Max  */}
         <div>
           <p className="text-3xl">
-            Max. {data.main.temp_max.toFixed(0)}
+            Max: {data.main.temp_max.toFixed(0)}
             °C
           </p>
         </div>
       </div>
-      {/* //! SUNRISE SUNSET
-      <div className=" mb-10 flex justify-between font-bold text-shadow">
-        //! Sunrise 
-        <div>
-          <p className="text-3xl">Sunrise: {data.sys.sunrise}</p>
-        </div>
-
-        //! Sunset 
-        <div>
-          <p className="text-3xl">Sunset: {data.sys.sunset} </p>
-        </div>
-      </div> */}
 
       {/*//! Container w/Overlay  */}
       <div className="bg-black/60 p-8 rounded-2xl shadow-lg shadow-black">
