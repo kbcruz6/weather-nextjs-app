b8e558b3cdbf67788b62ed79ac61bd7955168744
kbcruz6 <agustintcruz@gmail.com>
2022-12-26 10:34:08 -0300

    api k

--- components/Search.jsx
+++ components/Search.jsx
@@ -9,7 +9,7 @@ const Search = () => {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&units=metric`;
   // const url = `https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_KEY}&q=${city}&aqi=no`;
 
   //! API FETCH
